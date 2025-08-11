797630cbc28ceac958a22574442b281ef737a367
touriso <mayache@bocals-iMac.local>
2025-08-11 10:49:05 +0100

    ggg

---
+++ backend/airbcar_backend/airbcar_backend/settings_local.py
@@ -0,0 +1,27 @@
+"""
+Local development settings for airbcar_backend project.
+Use this for running tests and development outside of Docker.
+"""
+
+import sys
+from .settings import *
+
+# Override database settings for local development
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+# For testing, you can also use an in-memory SQLite database
+if 'test' in sys.argv:
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:'
+    }
+
+# For testing, we'll use a simpler file storage that doesn't require Pillow
+DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
+# Disable some features that might cause issues in testing
+MEDIA_ROOT = '/tmp/test_media/'
