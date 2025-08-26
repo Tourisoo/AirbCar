30ba04b5fe6eabb64e5ce818c8b605b1f9db896e
amine-za <aminezaghloul2003@gmail.com>
2025-08-26 13:29:58 +0100

    comments in api's

--- backend/airbcar_backend/airbcar_backend/urls.py
+++ backend/airbcar_backend/airbcar_backend/urls.py
@@ -29,9 +29,9 @@ from django.conf.urls.static import static
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
-router.register(r'partners', PartnerViewSet)
-router.register(r'listings', ListingViewSet)
-router.register(r'bookings', BookingViewSet)
+router.register(r'partners', PartnerViewSet) # add partner
+router.register(r'listings', ListingViewSet) # add a listing
+router.register(r'bookings', BookingViewSet) # add a booking
 
 
 urlpatterns = [
@@ -40,16 +40,16 @@ urlpatterns = [
     path('', include(router.urls)),
     path('api/users/list/', user_list, name='user_list'),
     path('api/bookings/list/', booking_list, name='bookings_list'),
-    path('api/token/', CustomLoginView.as_view(), name='token_obtain_pair'),
+    path('api/token/', CustomLoginView.as_view(), name='token_obtain_pair'), # sign-in
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/verify-token/', TokenVerifyView.as_view(), name='token_verify'),
     path('api/verify-admin/', AdminVerificationView.as_view(), name='admin_verify'),
-    path('api/register/', UserRegisterView.as_view(), name='user_register'),
+    path('api/register/', UserRegisterView.as_view(), name='user_register'), # sign-up
     path('api/password-reset/', PasswordResetRequestView.as_view(), name='password_reset_request'),
     path('api/reset-password/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('api/verify-email/', UserVerificationView.as_view(), name='user_verify_email'),
     path("verify-email/", verify_email, name="verify_email"),
-    path('api/profile/', UserProfileView.as_view(), name='user_profile')
+    path('api/profile/', UserProfileView.as_view(), name='user_profile') # update user - patch req 
     # path('api-auth/', include('rest_framework.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
