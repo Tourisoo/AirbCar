bedef44790d4afef7c75e5056307b47b6e20c404
amine-za <aminezaghloul2003@gmail.com>
2025-09-19 17:28:31 +0100

    enhancements

--- backend/airbcar_backend/airbcar_backend/urls.py
+++ backend/airbcar_backend/airbcar_backend/urls.py
@@ -1,20 +1,3 @@
-"""
-URL configuration for airbcar_backend project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
@@ -27,26 +10,25 @@ from django.conf.urls.static import static
 
 
 router = DefaultRouter()
-router.register(r'users', UserViewSet) # Get: get user info
-router.register(r'partners', PartnerViewSet) # Post: add partner
-router.register(r'listings', ListingViewSet) # Post: add a listing
-router.register(r'bookings', BookingViewSet, basename='booking') # Post: add a booking
-
+router.register(r'users', UserViewSet, basename='user')
+router.register(r'partners', PartnerViewSet, basename='partner')
+router.register(r'listings', ListingViewSet, basename='listing')
+router.register(r'bookings', BookingViewSet, basename='booking')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_view),
+    path('', home_view, name= 'home'),
     path('', include(router.urls)),
-    # path('api/bookings/list/', booking_list, name='bookings_list'),
-    path('api/login/', CustomLoginView.as_view(), name='login'), # sign-in - log-in
-    path('api/register/', UserViewSet.as_view({'post': 'create'}), name='user_register'), # sign-up
+   
+    path('api/register/', UserViewSet.as_view({'post': 'create'}), name='user_register'),
+    path('api/login/', CustomLoginView.as_view(), name='login'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/verify-token/', TokenVerifyView.as_view(), name='token_verify'),
     path('api/verify-admin/', AdminVerificationView.as_view(), name='admin_verify'),
+   
     path('api/password-reset/', PasswordResetRequestView.as_view(), name='password_reset_request'),
     path('api/reset-password/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+  
     path('api/verify-email/', UserVerificationView.as_view(), name='user_verify_email'),
     path("verify-email/", verify_email, name="verify_email"),
-    # path('api/profile/', UserProfileView.as_view(), name='user_profile') # update user - patch req 
-    # path('api-auth/', include('rest_framework.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
